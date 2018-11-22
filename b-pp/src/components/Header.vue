8ab51eafb1bc6936e277ac505e1c1ca3ef7b2a16
es5es5 <es5e5s@nate.com>
2018-11-23 01:02:26 +0900

    # header

---
+++ b-pp/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="header">
+        <div class="ui menu">
+            <a class="item">Browse</a>
+            <a class="item">Submit</a>
+            <div class="right menu">
+                <a class="item">Sign Up</a>
+                <a class="item">Help</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
---
+++ b-pp/src/components/MainComponents.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="main_components">
+		<Header></Header>
+
+        <div>main-components</div>
+
+		<transition name="fade" mode="out-in">
+			<router-view></router-view>
+		</transition>
+	</div>
+</template>
+
+<script>
+import Header from './../components/Header.vue'
+
+export default {
+	name: 'Main',
+	components: {
+		Header
+	},
+	methods: {}
+}
+</script>
+
+<style lang="scss">
+
+</style>
